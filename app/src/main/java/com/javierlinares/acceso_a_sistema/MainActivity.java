e6778b90741fb68c2e42526e18215b90b30f80fd
JavierLinares04 <peloto15@gmail.com>
2020-11-26 18:38:41 +0100

    Layaout terminado

---
+++ app/src/main/java/com/javierlinares/acceso_a_sistema/MainActivity.java
@@ -0,0 +1,14 @@
+package com.javierlinares.acceso_a_sistema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
